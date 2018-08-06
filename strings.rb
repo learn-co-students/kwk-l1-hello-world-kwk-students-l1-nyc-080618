54ac6e4303857b2645ad21f91ddf9dafc0d1cea5
audreybernstein <audrey.bernstein@aol.com>
2018-08-06 17:50:17 +0000

    Done.

---
+++ strings.rb
@@ -0,0 +1,8 @@
+puts "audrey"
+puts "audrey".upcase
+puts "audrey".downcase
+puts "audrey".reverse
+puts "audrey".swapcase
+puts "audrey".capitalize
+puts "audrey ".length
+puts "i love kode with klossy".upcase.reverse
